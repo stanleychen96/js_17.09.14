f787d63bdf964a51a4e3c8509aaaf593b4dcd3ff
stanleychen96 <stanleychen@yeah.net>
2017-09-14 13:58:40 +0800

    Verify array's length.

---
+++ length.js
@@ -0,0 +1,15 @@
+/**
+ * 2017-09-14       Stanley Chen
+ */
+
+var arr = [1, 2, 3];
+console.log(arr.length);
+
+arr.length = 6;
+console.log(arr);
+
+arr.length = 2;
+console.log(arr);
+
+arr.length = 3;
+console.log(arr);
