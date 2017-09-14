79c38abd493f281aa8420ce97e23990246ff7a67
stanleychen96 <stanleychen@yeah.net>
2017-09-14 20:19:47 +0800

    sequence of numbers

---
+++ for.js
@@ -0,0 +1,18 @@
+/**
+ * 2017-09-14 Stanley Chen
+ */
+
+
+ /**
+var fib = [1, 1];
+for(var n = 2; n <= 40; n++){
+    fib[n] = fib[n - 1] + fib[n - 2];
+}
+console.log(fib);
+*/
+
+var a = [1, 1];
+for(var m = 2; m <= 30; m++){
+    a[m] = a[m - 1] + 2 * a[m - 2];
+}
+console.log(a);
