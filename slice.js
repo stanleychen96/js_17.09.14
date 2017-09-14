f2e1cfac635e1e166ca95021d1c315e197b55b31
stanleychen96 <stanleychen@yeah.net>
2017-09-14 15:06:47 +0800

    slice & '$'

---
+++ slice.js
@@ -0,0 +1,29 @@
+/**
+ * 2017-09-14
+ */
+
+var arr = ['red', 'green', 'blue', 'yellow', 'pink', 'black', 'gray', 'puple'];
+
+console.log(`arr.slice(1, 3) =>! [! ${arr.slice(1, 3)} !]!`);
+console.log(`arr.slice(1, 3) => ${arr.slice(1, 3)} `);
+
+console.log(`[ ${ arr.slice(1, 3)} ]` );
+console.log(` ${arr.slice(1, 3)} ` );
+
+console.log(`${arr.slice(1, 3)} ` );
+
+/**
+console.log(arr.slice(1, 4));
+console.log(arr.slice(5, 10));
+
+console.log(arr.slice(10, 5));
+console.log(arr.slice(-1, 1));
+
+console.log(arr.slice(1, -1));
+console.log(arr.slice(2, -2));
+
+console.log(arr.slice(-2));
+console.log(arr.slice(4, -4));
+*/
+
+
