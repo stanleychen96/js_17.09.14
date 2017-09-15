91460294b6a9f855f0c33f370f6bae70f66a36ff
stanleychen96 <stanleychen@yeah.net>
2017-09-15 17:04:24 +0800

    driver

---
+++ driver.js
@@ -0,0 +1,65 @@
+/**
+ * 2017-09-15 Stanley Chen
+ */
+
+var devices = ['/dev/console', '/dev/display'];
+
+function open(dev){
+     var index = devices.indexOf(dev);
+
+     if(index < 0) {
+         console.log('not found');
+     } else {
+         drivers[index].open();
+     }
+     return index;
+};
+
+function write(dev, content){
+    return drivers[dev].write(content)
+};
+
+function read(dev){
+    return drivers[dev].read()
+};
+
+function close(dev){
+    return drivers[dev].close()
+};
+
+var drivers = [
+    {
+        type: '/dev/console',
+        open: function(){
+            console.log(`console.open`)
+        },
+        write: function(content){
+            console.log(`console.write ${content}`)
+        },
+        read: function(){
+            console.log('console.read')
+        },
+        close: function(){
+            console.log('console.close')
+        }
+    },
+    {
+        type: '/dev/display',
+        open: function(){
+            console.log('display.open')
+        },
+        write: function(content){
+            console.log(`display.write ${content}`)
+        },
+        read: function(){
+            console.log('display.read')
+        },
+        close: function(){
+            console.log('display.close')
+        }
+    }
+];
+
+var fd = open('/dev/display');
+write (fd, 'content');
+close (fd);
--- object.js
+++ object.js
@@ -3,24 +3,36 @@
  */
 
 var object = {
-    bool: 1 < 0,
-    value: 10,
-    fun: function (){
-        console.log(
-            `bool => ${this.bool}, 
-            value => ${this.value}, 
-            function => ${this.fun}`);
-    },
+bool: 1 < 0,
+value: 10,
+fun: function (){
+    console.log(
+        `bool => ${this.bool}, 
+        value => ${this.value}, 
+        function => ${this.fun}`);
+},
 };
 
 object.fun();
 
 object.bool = function() {
-    console.log('Hi');
+console.log('Hi');
 };
 
 object.value = function(arg) {
-    console.log(`arg => ${arg}`);
+console.log(`arg => ${arg}`);
 };
 
 object.fun(); // 调用属性;
+
+
+var like = {
+    boo: true,
+    val: 19,
+    fu: function (){
+        console.log(
+            `bool => ${this.boo},
+            value => ${this.val},
+            function => ${this.fu}`);
+        },
+    };
