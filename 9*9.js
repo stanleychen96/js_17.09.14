7b9ab11610e39338abdb2de73954f2da90651c81
stanleychen96 <stanleychen@yeah.net>
2017-09-18 17:08:39 +0800

    9*9

--- 9*9.js
+++ 9*9.js
@@ -12,12 +12,13 @@ for (var x = 1; x < 10; x++){
     console.log(`${arr}`);
 }
 */
-
+var empty = '';
+console.log(`${empty.padEnd(8,"_")}1________2_______3_______4_______5_______6_______7_______8_______9_______`);
 for (var x = 1; x < 10; x++){
     var line = '';
     for(var y = 1; y <= x; y++){
         line = line + `${x * y}\t`;
     }
-    line = `${x}\t` + line;
+    line = `${x}|\t` + line;
     console.log(line);
 }
