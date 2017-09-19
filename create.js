eaf67284b8a56b35b28395307e7ccca9d03d2e31
stanleychen96 <stanleychen@yeah.net>
2017-09-19 21:03:42 +0800

    create

--- create.js
+++ create.js
@@ -3,14 +3,46 @@
  */
 
 var person = {
-    age : 1,
-    apple : 2
+    age : 19,
+    name : true,
+    add : '1',
+    foo : function(){}
 };
 person.mounting = '1';
 
-var c = Object.create(person, {
-    age :{ value : '2'}
+var a = Object.create(person, {
+    age :{ 
+        value : 20}
 });
 
-console.log(c.__proto__);
-console.log(person.mounting);
+//console.log(a.__proto__);
+//console.log(person.mounting);
+//console.log(a.mounting);
+
+a.__proto__.name = 1;
+console.log(person.name);
+ 
+var b = Object.create(person,{});
+console.log(a.age, b.age);
+console.log(person.age);
+
+b.__proto__.age = 3;
+
+console.log(person.age);
+
+var c = Object.create(person,{
+    age : {
+        f : 10,
+        g : 20
+    },
+    name : { value : ['hello',1] },
+    add : { value : function(){} },
+    foo : { value : true }
+});
+
+console.log(person.add,person.age);
+console.log(person.name);
+console.log(typeof c.add);
+console.log(typeof c.age);
+console.log(typeof c.name);
+console.log(typeof c.foo)
