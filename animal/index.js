08499ec72a59f475c60d6cfd7527bb3ba98cf23d
stanleychen96 <stanleychen@yeah.net>
2017-09-21 20:41:53 +0800

    npm
    npm

---
+++ animal/index.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+const _ = require('lodash');
+
+/*
+var objects = [{ 'x': 3 }, {'j': 4 }, { 'j': 6 }, {'x   ': 6 }];
+console.log(_.sortedIndexBy(objects, { 'j': 7 }, 'j'));  only inspect which include element 'j'
+*/
+
+console.log(_.lastIndexOf([1, 2, 3, 2, 1, 2], 2));
